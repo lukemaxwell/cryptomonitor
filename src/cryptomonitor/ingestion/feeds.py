695667acc4a74f7131a6a03d967a362959bb1df9
Luke Maxwell <lukemaxwellshouse@gmail.com>
2022-09-08 14:40:26 +0100

    Remove stale code

--- src/cryptomonitor/ingestion/feeds.py
+++ src/cryptomonitor/ingestion/feeds.py
@@ -55,7 +55,6 @@ async def fetch_feeds(
     for feed in pending_feeds:
         tasks.append(fetch_feed(http_session=http_session, feed=feed))
     return await asyncio.gather(*tasks)
-    # return await asyncio.gather(*tasks, return_exceptions=True)
 
 
 def is_updated_feed(feed: models.Feed, parsed_feed: feedparser.FeedParserDict):
@@ -63,58 +62,6 @@ def is_updated_feed(feed: models.Feed, parsed_feed: feedparser.FeedParserDict):
     return feed.last_updated is None or feed_updated > feed.last_updated
 
 
-# async def fetch_articles(
-#     feed: models.Feed,
-#     parsed_feed: feedparser.FeedParserDict,
-# ) -> List[schemas.ArticleCreate]:
-#     """
-#     Return feed entries published after `feed.last_article_date`.
-#     """
-#     async with async_session() as db_session:
-#         if is_new_feed_version(feed=feed, parsed_feed=parsed_feed):
-#             feed_update = schemas.FeedUpdate(
-#                 last_updated=datetime.fromtimestamp(
-#                     mktime(parsed_feed.feed.updated_parsed)
-#                 ),
-#                 last_article_date=feed.last_article_date or datetime(1970, 1, 1),
-#             )
-#             last_article_date = feed_update.last_article_date
-#             for entry in parsed_feed["entries"]:
-#                 article_published = datetime.fromtimestamp(
-#                     mktime(entry.published_parsed)
-#                 )
-#                 if is_new_article(feed=feed, entry=entry):
-#                     if article_published > last_article_date:
-#                         last_article_date = article_published
-#                     if "content" not in entry:
-#                         article_job = article_job_from_entry(feed=feed, entry=entry)
-
-#                         try:
-#                             await article_crud.create_article_job(
-#                                 db_session=db_session, article_job=article_job
-#                             )
-#                             logger.info(f"Queued article job for {entry['link']}")
-#                         except IntegrityError:
-#                             logger.info(
-#                                 f"Article job already exists for {entry['link']}"
-#                             )
-#                             await db_session.rollback()
-#                     else:
-#                         article = article_from_entry(feed=feed, entry=entry)
-
-#                         if len(article.rules) > 0:
-#                             db_article = await article_crud.create_article(
-#                                 db_session=db_session, article=article
-#                             )
-#                             print(db_article.body)
-
-#             if last_article_date > feed_update.last_article_date:
-#                 feed_update.last_article_date = last_article_date
-
-#             await feed_crud.update_feed(feed_id=feed.id, feed_update=feed_update)
-
-
-# async def fetch_pending_feeds(db_session: AsyncSession):
 async def fetch_pending_feeds():
     """
     Fetch feed items for all pending feeds
